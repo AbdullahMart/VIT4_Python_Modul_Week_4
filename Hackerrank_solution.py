30de835a2e264a98f2ffe91238585d3adbdb948e
ERKHAN KOZHA <erkhankozha@gmail.com>
2024-04-18 11:53:09 +0200

    updated

--- Hackerrank_solution.py
+++ Hackerrank_solution.py
@@ -16,6 +16,50 @@ def rotateLeft(d, arr):
     return arr  
 
 #Question 3:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n):
+    answer= ["Louise","Richard"]
+    count = 0
+    summer = n
+    while summer!=1:
+        modulus = summer - 2**(math.log(summer)//math.log(2))
+        if modulus!=0:
+            summer = modulus
+        else:
+            summer = summer/2
+        if summer!=1:
+            count+=1
+    return answer[count%2]                
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
 
 
 
