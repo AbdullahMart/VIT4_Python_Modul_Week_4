86882f4fc00a41488f0788b0a6b3964a6bb2b17a
ERKHAN KOZHA <erkhankozha@gmail.com>
2024-04-19 12:56:00 +0200

    updated

--- Hackerrank_solution.py
+++ Hackerrank_solution.py
@@ -1,21 +1,5 @@
 
 
-
-#Question 1:
-
-
-
-#Question 2:
-
-def rotateLeft(d, arr):
-    for i in range(d):
-        temp=arr[0]
-        for j in range(len(arr)-1):
-            arr[j]=arr[j+1]
-        arr[len(arr)-1]=temp    
-    return arr  
-
-#Question 3:
 #!/bin/python3
 
 import math
@@ -32,18 +16,6 @@ import sys
 #
 
 def counterGame(n):
-    answer= ["Louise","Richard"]
-    count = 0
-    summer = n
-    while summer!=1:
-        modulus = summer - 2**(math.log(summer)//math.log(2))
-        if modulus!=0:
-            summer = modulus
-        else:
-            summer = summer/2
-        if summer!=1:
-            count+=1
-    return answer[count%2]                
     # Write your code here
 
 if __name__ == '__main__':
@@ -59,38 +31,3 @@ if __name__ == '__main__':
         fptr.write(result + '\n')
 
     fptr.close()
-
-
-
-
-
-#Question 4:
-import math
-import os
-import random
-import re
-import sys
-from datetime import datetime
-# Complete the time_delta function below.
-def time_delta(t1, t2):
-    format_ = '%a %d %b %Y %H:%M:%S %z'
-    t1 = datetime.strptime(t1, format_)
-    t2 = datetime.strptime(t2, format_)
-    return str(int(abs((t1-t2).total_seconds()))) 
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input())
-
-    for t_itr in range(t):
-        t1 = input()
-
-        t2 = input()
-
-        delta = time_delta(t1, t2)
-
-        fptr.write(delta + '\n')
-
-    fptr.close()
-
